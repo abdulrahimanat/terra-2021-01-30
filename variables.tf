286b014b1eab064bdaa70c9e4b8a603031510d3d
Abdul Rahiman AT <31490398+abdulrahimanat@users.noreply.github.com>
2021-02-27 22:08:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,21 @@ variable "region" {
 variable "inbound" {
   type        = list(map(string))
   description = "list of ingress ports"
-  default     = null
+  default     = [
+      {
+         description = "ssh from VPC"
+         from_port   = 22
+         to_port     = 22
+         protocol    = "tcp"
+         cidr_blocks = "10.0.0.0/21"
+      },
+      {
+         description = "http from VPC"
+         from_port   = 80
+         to_port     = 80
+         protocol    = "tcp"
+         cidr_blocks = "0.0.0.0/0"
+      }]
 }
 
 variable "sg_cidr" {
